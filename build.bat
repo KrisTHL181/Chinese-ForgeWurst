43704a9afe3bd69399d13153599dde70a37245c9
KrisTHL181 <KrisTHL181@outlook.com>
2024-06-21 21:00:24 +0800

    +AntiKnockback

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
 @echo off
 cd "ForgeWurst MC 1.12.2"
 echo === CURRENT DIRECTORY: %cd% ===
-rd /s /q .\build\
+echo === REMOVE BUILD DIRECTORY: %cd%\build\ ===
+del /f /s /q .\build\ > nul
 echo === BUILDING... ===
 echo === TARGET DIRECTORY: %cd%\build\libs\===
-gradlew clean build
+gradlew build
 echo === BUILD SUCCESS! ===
