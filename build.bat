eff44b4db5baf839508ea357dd3ec438b9671f15
KrisTHL181 <KrisTHL181@outlook.com>
2024-11-03 08:52:59 +0800

    修复AutoEZ功能

--- build.bat
+++ build.bat
@@ -1,10 +1,9 @@
 @echo off
 cd "ForgeWurst MC 1.12.2" 1>nul 2>nul
-if %ERRORLEVEL% equ 0 (echo === [+] CHANGE WORKING DIRECTORY [+ ]===)
+if %ERRORLEVEL% equ 0 (echo === [+] CHANGE WORKING DIRECTORY [+]===)
 echo === CURRENT DIRECTORY: %cd% ===
-echo === REMOVE BUILD DIRECTORY: %cd%\build\ ===
 del /f /s /q .\build\ 1>nul 2>nul nul
-if %ERRORLEVEL% equ 0 (echo === [+] CLEANED LAST BUILD [+ ]===)
+if %ERRORLEVEL% equ 0 (echo === [+] CLEANED LAST BUILD: %cd%\build\ [+] ===)
 echo === BUILDING... ===
 echo === TARGET DIRECTORY: %cd%\build\libs\===
 gradlew build %*
