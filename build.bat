195811c23f0dd5ff3336ed6ad6ea0db057667de8
KrisTHL181 <KrisTHL181@outlook.com>
2025-07-10 14:08:16 +0800

    略微修改编译脚本及.gitignore文件以更鲁棒和高速

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
 @echo off
 @chcp 65001 > nul
-@set %GRADLE_EXIT_CONSOLE% = true
-cd "ForgeWurst MC 1.12.2" 1>nul 2>nul
-if %ERRORLEVEL% equ 0 (echo === [+] CHANGE WORKING DIRECTORY [+]===)
-echo === CURRENT DIRECTORY: %cd% ===
-del /f /s /q .\build\ 1>nul 2>nul nul
+set GRADLE_EXIT_CONSOLE = 1
+cd /d "%~dp0ForgeWurst MC 1.12.2" >nul 2>&1
+if %ERRORLEVEL% equ 0 (echo === [+] CHANGE WORKING DIRECTORY [+] === )
+echo === CURRENT DIRECTORY: %~dp0 ===
+del /f /s /q .\build\ 1>nul 2>nul
 if %ERRORLEVEL% equ 0 (echo === [+] CLEANED LAST BUILD: %cd%\build\ [+] ===)
 echo === BUILDING... ===
 echo === TARGET DIRECTORY: %cd%\build\libs\===
